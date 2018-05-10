d35761e55d80b960da3b909f9ab2b0c16122cc83
gugafonsecaf <38438571+gugafonsecaf@users.noreply.github.com>
2018-05-09 21:33:18 -0300

    Create P4.c

---
+++ P4.c
@@ -0,0 +1,25 @@
+//Gustavo Fonseca Faria
+//11721EAU002
+# include <stdio.h>
+
+int main() 
+{
+	int cum=0, i;
+	char numero[256];
+
+	printf ("Digite um numero: ");
+	scanf ("%s", &numero);
+	getchar ();
+	
+	for (i=0; numero[i] != '\0'; i++)
+	{
+		if (numero[i] >=48 && numero [i] <= 57)
+		{
+			numero[i] = numero[i] - '0';
+			cum = cum*10 + numero[i];
+		}
+	}
+	
+	printf ("\nO numero digitado: %d", cum);
+	return 0;
+}
