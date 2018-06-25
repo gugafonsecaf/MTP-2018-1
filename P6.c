21ba6f92b3413c899786d8f0be8f48bdb5f2bfb6
gugafonsecaf <38438571+gugafonsecaf@users.noreply.github.com>
2018-06-25 11:31:51 -0300

    Update P6.c

--- P6.c
+++ P6.c
@@ -2,27 +2,44 @@
 //11721EAU002
 #include <stdio.h>
 
-int soma(int*, int*);
-float divide(float, float);
-int main ()
+int som(int num[], int a)
 {
-	int vet[20], i=0, k;
-	int *p, *q;
-	float j, i1;
-	printf("\nDigite um numero entre 5 e 20, que definira o tamanho do vetor: ");
-	do{	scanf("%d", &k);}while(k<5 || k>20);
-	do{ vet[i] = 0; i++; }while(i<k);
-	i=0;
-	while(i<k)
-	{	printf("\nDigite o valor %d do vetor: ", i+1);
-		scanf("%d", &vet[i]);	i++;	}
-	p = vet;	q = vet+k-1;	i=k;
-    j = float(soma(p, q));
-    i1 = float(i);
-	printf("\n\n Media = %.1f", divide(i, j));
-	return 0;
+	int soma = 0, i;
+	for(i = 0; i < a; i++)
+	{
+		soma = soma + num[i];
+	}
+	return soma;
 }
-int soma(int *p, int *q)
-{	return (*q) + ((q==p) ? 0 : soma(p, q-1));	}
-float divide(float p, float q)
-{	return q/p;	}
+float divisao(float soma, float a)
+{
+	float media;
+	media = soma / a;
+	return media;
+}
+int main()
+{
+	int a, j, num[20], resultado;
+	printf("\nDigite a quantidade de valores (entre 5 e 20): ");
+	scanf("%d", &a);
+	getchar();
+	if(a >= 5 && a <= 20)
+	{
+	    for(j = 0; j < a; j++)
+	    {	
+		    printf("Valor %d: ", j+1);
+		    scanf("%d", &num[j]);
+		    getchar();
+	    }
+    }
+    else
+    {
+    	printf("Numero digitado invalido. ");
+	}
+	while(a >= 5 && a <= 20)
+	{
+	    resultado = som(num, a);
+	    printf("\nMedia = %.2f", divisao(resultado, a));
+	    return 0;
+    }
+}
