580dff7d84a7938c9435465751b176d35169fcad
gugafonsecaf <38438571+gugafonsecaf@users.noreply.github.com>
2018-06-25 11:33:58 -0300

    Update P4.c

--- P4.c
+++ P4.c
@@ -1,25 +1,30 @@
 //Gustavo Fonseca Faria
 //11721EAU002
-# include <stdio.h>
 
-int main() 
+#include <stdio.h>
+
+int main ()
+
 {
-	int cum=0, i;
-	char numero[256];
 
-	printf ("Digite um numero: ");
-	scanf ("%s", &numero);
-	getchar ();
-	
-	for (i=0; numero[i] != '\0'; i++)
+char bits[256];
+int cum=0, i;
+
+printf ("Digite um numero: ");
+scanf ("%s", bits);
+getchar();
+
+for (i=0; bits[i] != '\0'; i++)
+{
+	if(bits[i] >=48 && bits[i]<=57)
 	{
-		if (numero[i] >=48 && numero [i] <= 57)
-		{
-			numero[i] = numero[i] - '0';
-			cum = cum*10 + numero[i];
-		}
+		bits[i]=bits[i]-'0';
+		cum= cum*10 + bits[i];
 	}
-	
-	printf ("\nO numero digitado: %d", cum);
-	return 0;
+
 }
+
+printf ("O numero digitado: %d", cum);
+return 0;
+}
+
